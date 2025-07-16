494c883c1d4061a750136b1c2d3d8295a4c97953
GaloViturco <viturco12@gmail.com>
2025-07-16 17:13:38 -0500

    fix: ⚙️update frontend URL to match NGINX route

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -14,7 +14,7 @@ function Login() {
   useEffect(() => {
     const fetchCaptcha = async () => {
       try {
-        const response = await axios.get('http://54.144.143.116/api/captcha', { responseType: 'blob' });
+        const response = await axios.get('http://54.144.143.116/api/captcha/generate-captcha', { responseType: 'blob' });
         
         // Crear una URL del objeto blob
         const imageUrl = URL.createObjectURL(response.data);
