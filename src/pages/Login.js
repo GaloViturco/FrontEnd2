7b9188b413814472d9b5eaf867dfce2ab580cb70
GaloViturco <viturco12@gmail.com>
2025-07-16 14:31:24 -0500

    chore: 🔧update frontend to use API Gateway routes

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -14,7 +14,7 @@ function Login() {
   useEffect(() => {
     const fetchCaptcha = async () => {
       try {
-        const response = await axios.get('http://54.144.143.116:5000/generate-captcha', { responseType: 'blob' });
+        const response = await axios.get('http://54.144.143.116/api/captcha', { responseType: 'blob' });
         
         // Crear una URL del objeto blob
         const imageUrl = URL.createObjectURL(response.data);
@@ -29,7 +29,7 @@ function Login() {
 
   const handleLogin = async () => {
     try {
-      const res = await axios.post('http://54.144.143.116:8001/login', {
+      const res = await axios.post('http://54.144.143.116/api/login', {
         email,
         password,
         captcha: captchaText // Incluir el captcha ingresado por el usuario
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -21,7 +21,7 @@ function Register() {
 
   const handleRegister = async () => {
     try {
-      const res = await axios.post('http://54.144.143.116:8002/register', {
+      const res = await axios.post('http://54.144.143.116/api/register', {
         email,
         password
       });
